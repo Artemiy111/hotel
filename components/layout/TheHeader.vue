84c6b64dc8cf8074f8f76d18a58fd1e392a3f7c3
Artemiy111 <mrart111@mail.ru>
2024-01-22 22:50:15 +0300

    chore: move The* components to ~/layout

---
+++ components/layout/TheHeader.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+const route = useRoute()
+
+const isMainPage = computed(() => route.fullPath === '/')
+
+const routes = [
+  {
+    title: 'Главная',
+    link: '/',
+  },
+  { title: 'Номера', link: '/rooms/1' },
+]
+</script>
+
+<template>
+  <header
+    class="z-100 w-full flex justify-between gap-2 p-4"
+    :class="[isMainPage ? 'fixed left-0 top-0 backdrop-blur-lg backdrop-brightness-75 text-white' : 'mb-8 border-b-2 border-surface-200']"
+  >
+    <span class="font-bold">Отель Ватриго</span>
+
+    <nav class="flex gap-5">
+      <NuxtLink
+        v-for="routeItem in routes"
+        :key="routeItem.link"
+        :to="routeItem.link"
+        class="[&.router-link-exact-active]:font-bold"
+      >
+        {{ routeItem.title }}
+      </NuxtLink>
+    </nav>
+
+    <div class="flex gap-4">
+      <NuxtLink class="flex items-center gap-2">
+        <div class="i-mingcute:telegram-line text-2xl" />
+      </NuxtLink>
+      <NuxtLink class="flex items-center gap-2">
+        <div class="i-mingcute:whatsapp-line text-2xl" />
+      </NuxtLink>
+      <NuxtLink href="tel:+79999999999" class="flex items-center gap-2">
+        <!-- <div class="i-mingcute:phone-line text-xl"></div> -->
+        <span>+7 999 999 99 99</span>
+      </NuxtLink>
+    </div>
+  </header>
+</template>
